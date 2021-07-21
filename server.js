094a6152169e58456660cd0692c5fc2a7a32994a
SL <samanthl795@gmail.com>
2021-07-20 20:13:26 -0700

    Add POST for api/notes

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const fs = require('fs');
+const bodyParser = require('body-parser');
+const generateUniqueId = require('generate-unique-id');
 
 const app = express();
 const PORT = 3001;
@@ -8,6 +10,7 @@ const path = require('path');
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(bodyParser.json());
 
 // Route for static files
 app.use(express.static('public'));
@@ -24,6 +27,33 @@ app.get('/api/notes', function(req, res) {
             res.end();
         } else {
             res.json(JSON.parse(data));
+        }
+    });
+});
+
+// Route for POST /api/notes
+app.post('/api/notes', function(req, res) {
+    var newNote = req.body;
+    // Assign a unique ID for the note
+    newNote.id = generateUniqueId();
+    // Save the note in the JSON file
+    fs.readFile('db/db.json', 'utf8', function (error, data) {
+        if (error) {
+            console.error(error);
+            res.end();
+        } else {
+            var allNotes = JSON.parse(data);
+            allNotes.push(newNote);
+            // Write out the notes
+            fs.writeFile('db/db.json', JSON.stringify(allNotes), function(err) {
+                if (err) {
+                    console.error(err);
+                    res.end();
+                } else {
+                    // Return the new note
+                    res.json(newNote);
+                }
+            });
         }
     });
 });
